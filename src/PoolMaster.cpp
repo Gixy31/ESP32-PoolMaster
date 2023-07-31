84a0f79cf2bbf1b69a252a195ac5e68c00385d7d
Gixy31 <dnslfrcd@gmail.com>
2023-07-31 19:38:30 +0200

    Publish settings after filtration duration computation

--- src/PoolMaster.cpp
+++ src/PoolMaster.cpp
@@ -17,6 +17,7 @@ bool saveParam(const char*,double );
 void SetPhPID(bool);
 void SetOrpPID(bool);
 void mqttErrorPublish(const char*);
+void publishsettings(void);
 void UpdateTFT(void);
 void stack_mon(UBaseType_t&);
 void Send_IFTTTNotif(void);
@@ -121,6 +122,8 @@ void PoolMaster(void *pvParameters)
 
         Debug.print(DBG_INFO,"Filtration duration: %dh",storage.FiltrationDuration);
         Debug.print(DBG_INFO,"Start: %dh - Stop: %dh",storage.FiltrationStart,storage.FiltrationStop);
+
+        publishsettings();
 
         d_calc = true;
     }
